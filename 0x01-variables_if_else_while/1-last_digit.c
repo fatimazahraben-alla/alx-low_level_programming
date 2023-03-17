e47a985dffe835640e6519aad1f8da79cdd9fd14
fatimazahraben-alla <fatisafae33@gmail.com>
2023-03-17 10:02:53 -0700

    ADD README.md, 0-positive_or_negative.c, 1-last_digit.c and 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ *main - compare the last digit of a var
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int ld = n % 10;
+
+	if (ld > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, ld);
+	}
+	else if (ld == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, ld);
+	}
+	else if (ld != 0 && ld < 6)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+	}
+	return (0);
+}
