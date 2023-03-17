12346c245016ea51b631ed50da3846438b42c382
fatimazahraben-alla <fatisafae33@gmail.com>
2023-03-17 11:51:17 -0700

    ADD 3-print_alphabets.c, 4-print_alphabt.c and 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - print the alphabet in lowercase, and then in uppercase
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
