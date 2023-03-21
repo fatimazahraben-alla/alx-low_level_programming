cf17c3d249b7ee5be247e82b50c232c65281f3f7
fatimazahraben-alla <fatisafae33@gmail.com>
2023-03-21 12:23:54 -0700

    ADD _putchar.c and 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- *main - print _putchar 
+ *main - print _putchar
  *
  *Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
-	char string [8] = "_putchar";
+	char string[8] = "_putchar";
 
 	for (i = 0; i < 9; i++)
 	{
 		_putchar(string[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
