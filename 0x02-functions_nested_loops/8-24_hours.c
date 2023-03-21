c23171c5133ffd84ddec40ec45c19a6828ea5a3c
fatimazahraben-alla <fatisafae33@gmail.com>
2023-03-21 16:05:51 -0700

    ADD 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ *Return: void (Success)
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((m / 10) + 48);
+			_putchar((m % 10) + 48);
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
