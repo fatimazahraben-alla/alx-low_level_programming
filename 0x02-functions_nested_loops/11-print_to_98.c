82e2ddee3f5f1882ba922cf8aad5d88d02ec7335
fatimazahraben-alla <fatisafae33@gmail.com>
2023-03-21 20:12:26 -0700

    ADD 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,7 +15,11 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d ", i);
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			printf("%d, ", i);
 		}
 		_putchar('\n');
 	}
@@ -23,13 +27,17 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d ", i);
+			if (i == 98)
+			{
+				printf("%d", i);
+			}
+			printf("%d, ", i);
 		}
 		_putchar('\n');
 	}
 	else if (n == 98)
 	{
-		printf("%d ", n);
+		printf("%d", n);
 		_putchar('\n');
 	}
 }
