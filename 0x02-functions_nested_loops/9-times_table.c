4e549b0eb4e3d98eb6220d5fd4f8d71e3414b281
fatimazahraben-alla <fatisafae33@gmail.com>
2023-03-21 17:48:09 -0700

    ADD 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ *times_table - prints the 9 times table, starting with 0
+ *
+ *Return: void (Success)
+ */
+void times_table(void)
+{
+	int c, i;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (i = 0; i < 10; i++)
+		{
+			if (i == 0)
+			{
+				_putchar(c * i + '0');
+			}
+			if (c * i < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c * i + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((c * i / 10) + '0');
+				_putchar((c * i % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
