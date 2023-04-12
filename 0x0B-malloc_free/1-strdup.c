46cbc25d50d850ec0a5308ee0050f3f239b0efb2
fatimazahraben-alla <fatisafae33@gmail.com>
2023-04-12 08:38:05 -0700

    ADD 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+/**
+ *strlength - len of string
+ *@s: string
+ *Return: len
+ */
+int strlength(char *s)
+{
+	int i = 1;
+
+	while (s)
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
+/**
+ *_strdup - returns a pointer which contains a copy of the str
+ *@str: string
+ *Return: strdup
+ */
+char *_strdup(char *str)
+{
+	char *strdup = malloc(strlenght * sizeof(char));
+	int i;
+
+	if (!str)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < strlength; i++)
+	{
+		strdup[i] = str[i];
+	}
+	return (strdup);
+}
