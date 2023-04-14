a06037a06b3891839ec9e26d32e65595e0c35e3d
fatimazahraben-alla <fatisafae33@gmail.com>
2023-04-13 18:44:08 -0700

    ADD 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,7 +7,7 @@
  */
 int *array_range(int min, int max)
 {
-	int i, j = 0;
+	int i;
 	int *arr = malloc(sizeof(int) * (max - min + 1));
 
 	if (!arr || min > max)
@@ -16,8 +16,7 @@ int *array_range(int min, int max)
 	}
 	for (i = min; i <= max; i++)
 	{
-		arr[j] = i;
-		j++;
+		arr[i - min] = i;
 	}
 	return (arr);
 }
