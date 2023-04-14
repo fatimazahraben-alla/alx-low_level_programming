4610209cfc5707a05f9dc7e2e2d76c6ff06d95fd
fatimazahraben-alla <fatisafae33@gmail.com>
2023-04-13 18:13:35 -0700

    ADD 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,17 +7,11 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i = 0;
 	int *arr = malloc(nmemb * size);
 
 	if (!arr || nmemb == 0 || size == 0)
 	{
 		return (NULL);
-	}
-	while (i < nmemb * size)
-	{
-		arr[i] = 0;
-		i++;
 	}
 	return (arr);
 }
