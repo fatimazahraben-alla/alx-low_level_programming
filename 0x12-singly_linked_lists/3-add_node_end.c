35884e7fbeb65b8a6eec6baf1ea4878be32e3928
fatimazahraben-alla <fatisafae33@gmail.com>
2023-04-28 14:32:14 -0700

    ADD 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ *_strlen - returns the length of a string
+ *@s: arg
+ *Return: len
+ */
+unsigned int _strlen(const char *s)
+{
+        unsigned int len, i;
+
+        len = i = 0;
+        while (s[i] != '\0')
+        {
+                len++;
+                i++;
+        }
+        return (len);
+}
+/**
+ *add_node - adds a new node at the end of a list_t list
+ *@head: head
+ *@str: data
+ *Return: address of the new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newhead, *newnode = malloc(sizeof(list_t));
+
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->str = strdup(str);
+	newnode->len = _strlen(str);
+	newnode->next = NULL;
+	newhead = *head;
+	while (newhead != NULL)
+	{
+		newhead = newhead->next;
+	}
+	newhead->next = newnode;
+	return (newnode);
+}
