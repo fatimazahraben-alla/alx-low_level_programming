5d86db37e092d7bc59f1396b8bb8050ed2780313
fatimazahraben-alla <fatisafae33@gmail.com>
2023-05-04 16:29:27 -0700

    ADD 1-print_binary.c and 2-get_bit.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,3 +1,26 @@
 #include "main.h"
+/**
+ *print_binary - prints the binary representation of a number
+ *@n: number
+ *Return: void
+ */
 void print_binary(unsigned long int n)
 {
+	unsigned long int m = 1;
+
+	if (n == 0)
+		_putchar('0');
+	while (m <= n)
+		m <<= 1;
+	if (m > n)
+		m >>= 1;
+	while (m)
+	{
+		if (n & m)
+			_putchar('1');
+		else
+			_putchar('0');
+		m >>= 1;
+	}
+}
+
