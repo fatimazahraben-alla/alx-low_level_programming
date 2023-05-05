57643d79e0f6531cb1dce92ea0d623c157200656
fatimazahraben-alla <fatisafae33@gmail.com>
2023-05-04 17:28:46 -0700

    UPD 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,15 +9,18 @@ void print_binary(unsigned long int n)
 	unsigned long int m = 1UL << 63;
 
 	if (n == 0)
+	{
 		_putchar('0');
+		return;
+	}
 	while ((m & n) == 0)
 		m >>= 1;
 	while (m)
 	{
-		if (n & m)
-			_putchar('1');
-		else
+		if ((n & m) == 0)
 			_putchar('0');
+		else
+			_putchar('1');
 		m >>= 1;
 	}
 }
