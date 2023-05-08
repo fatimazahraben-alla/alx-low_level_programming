a402bee3d4ef2b61989af2c3bfc43cdab2c9cfca
fatimazahraben-alla <fatisafae33@gmail.com>
2023-05-08 15:53:37 -0700

    UPD 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	while (text_content[len])
 		len++;
-	if (text_content != NULL)
+	if (text_content == NULL)
+	{
+		close(ff);
+		return (1);
+	}
+	else
 	{
 		wbytes = write(ff, text_content, len);
 		if (wbytes == -1)
