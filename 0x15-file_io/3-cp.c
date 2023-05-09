818bcd4e8abfac509fcea675c9a086e93c3552c0
fatimazahraben-alla <fatisafae33@gmail.com>
2023-05-08 18:46:47 -0700

    UPD 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
 	}
-	f_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0666);
+	f_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (f_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]),
